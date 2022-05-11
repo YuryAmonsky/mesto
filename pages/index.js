be545650a53a85e997d479e6538dce13d205d08e
Yury Amonsky <yury.amonsky@yandex.ru>
2022-05-11 23:14:07 +0300

    feat: запрограммировать поведение элементов формы.

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.edit-profile');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let formCloseIcon = document.querySelector('.form-close-icon');
+let formInputName = document.querySelector('.edit-profile__name');
+let formInputAboutMe = document.querySelector('.edit-profile__about-me');
+let textProfileName = document.querySelector('.profile__name');
+let textProfileAboutMe = document.querySelector('.profile__about-me')
+
+
+function openForm() {  
+  formInputName.value = textProfileName.textContent;
+  formInputAboutMe.value = textProfileAboutMe.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
+
+function saveData(evt) {
+  evt.preventDefault();
+  textProfileName.textContent = formInputName.value;  
+  textProfileAboutMe.textContent = formInputAboutMe.value;  
+  closeForm();
+}
+
+profileEditButton.addEventListener('click', openForm);
+formCloseIcon.addEventListener('click', closeForm);
+formElement.addEventListener('submit', saveData);
