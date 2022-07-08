4229a685a9516bee517f81d2d3e478489b49f6a3
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-08 15:25:55 +0300

    feat: добавить в классе Section публичный метод для отрисовки  элементов

--- script/Section.js
+++ script/Section.js
@@ -3,5 +3,11 @@ export default class Section{
     this._items = items;
     this._renderer = renderer;
     this._container = document.querySelector(selectorContainer);
-  }   
+  }
+  /*метод отрисовки элементов*/
+  renderItems(){
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
 }
