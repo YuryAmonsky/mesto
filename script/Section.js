c8463f7711dbcfa7d7b959dd4fcedf402a0568ae
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-08 14:54:28 +0300

    feat: добавить в классе Section конструктор

--- script/Section.js
+++ script/Section.js
@@ -1,3 +1,7 @@
 export default class Section{
-  
+  constructor({items, renderer}, selectorContainer){
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(selectorContainer);
+  }   
 }
