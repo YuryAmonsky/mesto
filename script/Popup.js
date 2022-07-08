e8afbf1a7fffd85a256b1fa6b8877b8d9397efee
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-08 16:03:49 +0300

    feat: добавить в классе Popup метод  закрытия попапа

--- script/Popup.js
+++ script/Popup.js
@@ -1,14 +1,25 @@
 export default class Popup{
   constructor({selectorPopup, classOpenedPopup}){
-    this._element = document.querySelector(selectorPopup);
-    this._classOpened = classOpenedPopup;
+    this._element = document.querySelector(selectorPopup);    
+    this._classOpened = classOpenedPopup;/*class = 'popup_opened' */
   }
   
   /*открытие попапа*/
-  /*class = 'popup_opened' */
   open(){
     this._element.classList.add(this._classOpened);    
   }
 
+  /*закрытие попапа*/
+  close(){
+    this._element.classList.remove(this._classOpened);
+  }
+
+  //закрытие попапов
+  /*
+ function closePopup(popup) {
+  popup.removeEventListener('click', closePopupOnBGClick);
+  document.removeEventListener('keydown', closePopupOnEscapePress);
   
+}*/
+
 }
