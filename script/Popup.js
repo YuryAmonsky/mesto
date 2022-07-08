85fc4f38ba20b77862c7ee280f95b72d378ef14d
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-08 15:53:54 +0300

    feat: добавить в классе Popup открытия попапа

--- script/Popup.js
+++ script/Popup.js
@@ -3,4 +3,10 @@ export default class Popup{
     this._element = document.querySelector(selectorPopup);
   }
   
+  /*открытие попапа*/
+  /*class = 'popup_opened' */
+  open(classOpenElement){
+    this._element.classList.add(classOpenElement);    
+  }
+
 }
