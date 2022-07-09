c2e263cc281ec1a86873655efeb002704445e2ee
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-09 20:24:32 +0300

    feat: добавить в класс PopupWithForm конструктор

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -1,5 +1,9 @@
 import Popup from './Popup.js';
 
 export default class PopupWithForm extends Popup{
-  
+  constructor(objPopupClassHolder, objFormClassHolder, handleSubmit){
+    super(objPopupClassHolder);
+    this._elementForm = this._element.queryselector(objFormClassHolder.selectorForm);
+    this._handleSubmit =  handleSubmit;
+  }
 }
