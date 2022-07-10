dc37fc91121fa875610b6d0af6f6565288077e97
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-10 10:51:22 +0300

    feat: добавить в класс PopupWithForm метод, который собирает значения всех полей формы

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -4,11 +4,14 @@ export default class PopupWithForm extends Popup{
   constructor(objPopupClassHolder, objFormClassHolder, handleSubmit){
     super(objPopupClassHolder);
     this._elementForm = this._element.queryselector(objFormClassHolder.selectorForm);
+    this._inputsList = Array.from(this._elementForm.querySelectorAll(objFormClassHolder.selectorInput);
     this._elementSubmitButton = this._element.queryselector(objFormClassHolder.selectorSubmitButton);
     this._handleSubmit =  handleSubmit;
   }
 
   _getInputValues(){
-    
+    const inputsValues = [];
+    this._inputsList.forEach((item)=>inputsValues.push(item.value));
+    return inputsValues;
   }
 }
