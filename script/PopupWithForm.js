e76184bfd91cdc88cd3f880ac14c48bfdb6b526b
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-10 10:37:22 +0300

    fix: добавить в конструктор класса PopupWithForm свойство - элемент кнопки сабмита

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -4,6 +4,11 @@ export default class PopupWithForm extends Popup{
   constructor(objPopupClassHolder, objFormClassHolder, handleSubmit){
     super(objPopupClassHolder);
     this._elementForm = this._element.queryselector(objFormClassHolder.selectorForm);
+    this._elementSubmitButton = this._element.queryselector(objFormClassHolder.selectorSubmitButton);
     this._handleSubmit =  handleSubmit;
+  }
+
+  _getInputValues(){
+    
   }
 }
