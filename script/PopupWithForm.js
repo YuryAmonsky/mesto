f9c558c1df0cd82de4f0bc7fd10f7418e9111a14
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-13 18:38:25 +0300

    feat: переписать в классе PopupWithForm методы установки ис нятия слушателей

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -4,14 +4,24 @@ export default class PopupWithForm extends Popup{
   constructor(objPopupClassHolder, objFormClassHolder, handleSubmit){
     super(objPopupClassHolder);
     this._elementForm = this._element.queryselector(objFormClassHolder.selectorForm);
-    this._inputsList = Array.from(this._elementForm.querySelectorAll(objFormClassHolder.selectorInput);
+    this._inputsList = Array.from(this._elementForm.querySelectorAll(objFormClassHolder.selectorInput));
     this._elementSubmitButton = this._element.queryselector(objFormClassHolder.selectorSubmitButton);
     this._handleSubmit =  handleSubmit;
   }
 
   _getInputValues(){
     const inputsValues = [];
-    this._inputsList.forEach((item)=>inputsValues.push(item.value));
+    this._inputsList.forEach(item => inputsValues.push(item.value));
     return inputsValues;
+  }
+
+  setEventListeners(){
+    super.setEventListeners();
+    this._elementForm.addEventListener('submit', this._handleSubmit.bind(this));
+  }
+
+  removeEventListeners(){
+    super.removeEventListeners();
+    this._elementForm.removeEventListener('submit', this._handleSubmit);
   }
 }
