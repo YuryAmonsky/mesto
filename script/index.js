9da91fc9df34c26200bfb6fa1778ca0b9ead2416
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-14 06:27:22 +0300

    refactor: изменить импорты в index.js

--- script/index.js
+++ script/index.js
@@ -1,57 +1,14 @@
 //Подключение модулей
 //---------------------
-import{Card} from './card.js';
-import{FormValidator} from './FormValidator.js';
+import UserInfo from './UserInfo.js';
+import Card from '../components/Сard.js';
+import FormValidator from '../utils/FormValidator.js';
+import Section from './Section.js';
+import PopupWithForm from './PopupWithForm.js';
+import PopupWithImage from './PopupWithImage.js';
 
 //Объявление глобальных переменных и констант
 //----------------------
-
-const initialCards = [
-  {
-    name: 'Карачаево-Черкессия',
-    link: 'images/content/Karachaevsk.jpg',
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: 'images/content/Elbrus.jpg',
-  },
-  {
-    name: 'Сочи',
-    link: 'images/content/Sochi.jpg',
-  },
-  {
-    name: 'Алтай',
-    link: 'images/content/Altay.jpg',
-  },
-  {
-    name: 'Телецкое озеро',
-    link: 'images/content/Teletskoe_ozero.jpg',
-  },
-  {
-    name: 'Шато Эркен, Кабардино-балкария',
-    link: 'images/content/Shato_Erken_Nalchik.jpg',
-  }
-];
-
-const objFormElementsClassHolder = {
-  selectorForm: '.dialog-form',
-  selectorInput: '.dialog-form__input',
-  selectorSubmitButton: '.dialog-form__submit-button',
-  classButtonDisabled: 'dialog-form__submit-button_disabled',
-  classInputInvalid: 'dialog-form__input_invalid',
-  classErrorActive: 'dialog-form__input-error_active'
-};
-
-const objCardElementsClassHolder = {
-  selectorTemplate: '.location-template',
-  selectorCard: '.location',
-  selectorImage: '.location__image',
-  selectorName: '.location__name',
-  selectorButtonDelete: '.location__delete-icon',
-  selectorButtonLike: '.location__like',
-  selectorPopup: '.popup_type_view-image',  
-  classLike: 'location__like_active'
-}
 
 const textProfileName = document.querySelector('.profile__name');
 const textProfileAboutMe = document.querySelector('.profile__about-me');
