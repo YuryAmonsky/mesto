ddecfff810cdac413c1353a19b75b4422afe39cb
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-14 05:52:44 +0300

    feat: добавить в класс UserInfo конструктор, методы получения и записи данных и установки слушателей на кнопки

--- script/UserInfo.js
+++ script/UserInfo.js
@@ -1,3 +1,24 @@
-export class UserInfo{
-  
+export default class UserInfo{
+  constructor({selectorInputName, selectorInputAboutMe}, handleClickButtonEditProfile, handleClickButtonNewLocation){
+    this._elementProfileName = document.querySelector('.profile__name');
+    this._elementProfileAboutMe = document.querySelector('.profile__about-me');
+    this._buttonEditProfile = document.querySelector('.profile__edit-button');
+    this._buttonNewLocation = document.querySelector('.profile__add-button');
+    this._handleClickButtonEditrofile = handleClickButtonEditProfile;
+    this._handleClickButtonNewLocation = handleClickButtonNewLocation;
+  }
+
+  getUserInfo(){
+   return {textProfileName:this._elementProfileName.textContent, textProfileAboutMe:this._elementProfileAboutMe.textContent};
+  }
+
+  setUserinfo({textProfileName, textProfileAboutMe}){
+    this._elementProfileName.textContent = textProfileName;
+    this._elementProfileAboutMe.textContent = textProfileAboutMe;
+  }
+
+  setEventListeners(){
+    this._buttonEditProfile.addEventListener('click', this._handleClickButtonEditrofile.bind(this));
+    this._buttonNewLocation.addEventListener('click', this._handleClickButtonNewLocation.bind(this));
+  }
 }
