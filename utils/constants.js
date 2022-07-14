afc2b89b9aa9b15b93520ff35070aaf4feeb6348
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-14 06:23:50 +0300

    refactor: добавить папки для компонентов и утилит, перенести компоненты из script, добавить модуль с константами

---
+++ utils/constants.js
@@ -0,0 +1,46 @@
+const initialCards = [
+  {
+    name: 'Карачаево-Черкессия',
+    link: 'images/content/Karachaevsk.jpg',
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: 'images/content/Elbrus.jpg',
+  },
+  {
+    name: 'Сочи',
+    link: 'images/content/Sochi.jpg',
+  },
+  {
+    name: 'Алтай',
+    link: 'images/content/Altay.jpg',
+  },
+  {
+    name: 'Телецкое озеро',
+    link: 'images/content/Teletskoe_ozero.jpg',
+  },
+  {
+    name: 'Шато Эркен, Кабардино-балкария',
+    link: 'images/content/Shato_Erken_Nalchik.jpg',
+  }
+];
+
+const objFormElementsClassHolder = {
+  selectorForm: '.dialog-form',
+  selectorInput: '.dialog-form__input',
+  selectorSubmitButton: '.dialog-form__submit-button',
+  classButtonDisabled: 'dialog-form__submit-button_disabled',
+  classInputInvalid: 'dialog-form__input_invalid',
+  classErrorActive: 'dialog-form__input-error_active'
+};
+
+const objCardElementsClassHolder = {
+  selectorTemplate: '.location-template',
+  selectorCard: '.location',
+  selectorImage: '.location__image',
+  selectorName: '.location__name',
+  selectorButtonDelete: '.location__delete-icon',
+  selectorButtonLike: '.location__like',
+  selectorPopup: '.popup_type_view-image',  
+  classLike: 'location__like_active'
+}
