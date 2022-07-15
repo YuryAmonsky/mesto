3f12e10b55b6ced73c7fdf5b544a7262709589b6
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-15 10:03:29 +0300

    fix: отладка, исправить получение елемента формы в конструкторе класса PopupWithForm.

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -3,7 +3,7 @@ import Popup from './Popup.js';
 export default class PopupWithForm extends Popup{
   constructor(objPopupClassHolder, objFormClassHolder, handleSubmit){
     super(objPopupClassHolder);
-    this._elementForm = this._element.queryselector(objFormClassHolder.selectorForm);
+    this._elementForm = this._element.querySelector(objFormClassHolder.selectorForm);
     this._inputsList = Array.from(this._elementForm.querySelectorAll(objFormClassHolder.selectorInput));
     this._elementSubmitButton = this._element.queryselector(objFormClassHolder.selectorSubmitButton);
     this._handleSubmit =  handleSubmit;
