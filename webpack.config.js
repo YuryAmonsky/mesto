2c3cb6e369d2becfab28aa98fc74c2d97da33e7e
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-15 20:02:56 +0300

    feat: подключить плагин очистки в webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: {main: './src/index.js'},
   output: {
@@ -33,5 +34,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html' // путь к файлу index.html
     }),
+    new CleanWebpackPlugin(),
   ]
 }
