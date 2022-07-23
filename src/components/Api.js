2cb5f610b830b6619ba708aa4576aeec07a5349b
Yury Amonsky <yury.amonsky@yandex.ru>
2022-07-23 16:20:39 +0300

    добавить в класс Api метод получения с сервера данных пользователя

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,4 +3,20 @@ export default class Api{
     this.baseUrl = options.baseUrl;
     this.headers = options.headers;
   }
+
+  getUserInfo(){
+    return fetch(this.baseUrl+'/users/me ', {
+      method: "GET",
+      headers: this.headers
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err); // выведем ошибку в консоль
+      });
+  }
 }
